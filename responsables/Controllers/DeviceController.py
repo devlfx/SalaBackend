4bd2bd361deb50619bb488a36722740066894185
abe <laog18@gmail.com>
2021-08-15 00:03:35 -0500

    Agregar dispositivo con token de autenticacion

--- responsables/Controllers/DeviceController.py
+++ responsables/Controllers/DeviceController.py
@@ -5,16 +5,19 @@ from sqlalchemy.orm import Session
 from Database import get_db
 from Database.models import models
 from responsables.Schemas.Create import DispositivoResponsableCreate
-
+from fastapi_jwt_auth import AuthJWT
 
 
 class DeviceController:
-    def __init__(self,db :Session = Depends(get_db)):
+    def __init__(self, db:Session = Depends(get_db),AuthJWT:AuthJWT = Depends()):
         self.db = db
+        self.auth_jwt = AuthJWT
 
     def register_device_token(self,device:DispositivoResponsableCreate):
-        print(device)
+        self.auth_jwt.jwt_required()
+        user_data = self.auth_jwt.get_raw_jwt()
         data = device.dict()
+        data["id_responsable"] = user_data["id_responsable"]
         device_db = models.DispositivoResposable(**data)
         self.db.add(device_db)
         self.db.commit()
